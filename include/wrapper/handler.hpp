c3b5d1e60d94554e9f7613679e8a60d4cac39f15
Hugo Haldi <hugo.haldi@hsrv.cf>
2019-06-22 19:15:51 +0200

    Handle queries in url ('?query=value') to sort result

--- include/wrapper/handler.hpp
+++ include/wrapper/handler.hpp
@@ -18,7 +18,7 @@ namespace http = beast::http;
 constexpr std::string_view bsv2stdsv(boost::string_view bsv) noexcept { return {bsv.data(), bsv.length()}; }
 
 template <class Body, class Allocator> rapidjson::StringBuffer handle_json(http::request<Body, http::basic_fields<Allocator>>&& req) {
-    //    auto target = TargetParser{bsv2stdsv(req.target())};
+    auto target = TargetParser{bsv2stdsv(req.target())};
 
     rapidjson::Document json_res{};
     json_res.SetObject();
@@ -44,13 +44,13 @@ template <class Body, class Allocator> rapidjson::StringBuffer handle_json(http:
 
         std::string dom_str{};
         if (req.target().starts_with("/libvirt/domains/by-uuid/")) {
-            dom_str = std::string{req.target().substr(25)};
+            dom_str = target.getPath().substr(25);
             search_key = SearchKey::by_uuid;
         } else if (req.target().starts_with("/libvirt/domains/by-name/")) {
-            dom_str = std::string{req.target().substr(25)};
+            dom_str = target.getPath().substr(25);
             search_key = SearchKey::by_name;
-        } else if (!req.target().substr(16).empty() && !req.target().substr(17).empty()) {
-            dom_str = std::string{req.target().substr(17)};
+        } else if (!target.getPath().substr(16).empty() && !target.getPath().substr(17).empty()) {
+            dom_str = target.getPath().substr(17);
             search_key = SearchKey::by_name;
         }
 
@@ -142,7 +142,8 @@ template <class Body, class Allocator> rapidjson::StringBuffer handle_json(http:
                     res_val.AddMember("name", rapidjson::Value(dom.getName(), json_res.GetAllocator()), json_res.GetAllocator());
                     res_val.AddMember("uuid", dom.getUUIDString(), json_res.GetAllocator());
                     res_val.AddMember("status", rapidjson::StringRef(virt::Domain::States[info.state]), json_res.GetAllocator());
-                    jResults.PushBack(res_val, json_res.GetAllocator());
+                    if (!target["name"].empty() && target["name"].compare(dom.getName()) == 0)
+                        jResults.PushBack(res_val, json_res.GetAllocator());
                     json_res["success"] = true;
                 }
             }
@@ -173,9 +174,9 @@ template <class Body, class Allocator> rapidjson::StringBuffer handle_json(http:
             return error(10, "Failed to open connection to LibVirtD");
         }
         if (req.target().starts_with("/libvirt/domains"))
-            domains(std::move(conn));
+            return domains(std::move(conn));
         else if (req.target().starts_with("/libvirt/networks"))
-            networks(std::move(conn));
+            return networks(std::move(conn));
         else
             return error(2, "Bad URL");
     }();
