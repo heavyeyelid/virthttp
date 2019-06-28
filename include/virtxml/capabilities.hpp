6721cf8acd7dc65f189ff1bcfc7b0ba837dabb24
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-06-28 15:50:28 +0200

    virtxml: better file naming virtxml: Capabilities: Added Model string support for host cpu

--- include/virtxml/capabilities.hpp
+++ include/virtxml/capabilities.hpp
@@ -5,7 +5,7 @@
 #include <gsl/gsl>
 #include "generic.hpp"
 #include "xmlspan.hpp"
-#include "xmlstr.hpp"
+#include "xmlval.hpp"
 
 namespace virtxml {
 using namespace rapidxml_ns;
@@ -15,6 +15,16 @@ struct DriverCapabilities {
   public:
     struct Host : public Node {
         struct Cpu : public Node {
+            struct Model : public Node {
+#if 1 /* With functions */
+                constexpr explicit operator String() const noexcept { return String{node}; }
+                Optional<String> fallback_str() const noexcept { return String{node->first_attribute("fallback")}; }
+#else /* With paramexpr */
+                using operator String(this s) = String{s.node};
+                using fallback_str() = String{node->first_attribute("fallback")};
+#endif
+            };
+
             struct Feature;
             struct FeatureList : public NamedSpan<Feature> {
                 constexpr explicit FeatureList(xml_node<>* node) : NamedSpan<Feature>("feature", node) {}
@@ -28,7 +38,7 @@ struct DriverCapabilities {
 #endif
             };
 
-            struct Features; // NOT IMPLEMENTED YET // requires autogen
+            struct Features; // NOT IMPLEMENTED YET // requires autogen from https://github.com/wiedi/libvirt/blob/master/src/cpu/cpu_map.xml
 
             struct MigrationFeatures : public Node {
                 struct UriTransport : public String {};
@@ -45,7 +55,17 @@ struct DriverCapabilities {
 #endif
             };
 
-            struct Topology; // TODO
+            struct Topology : public Node {
+#if 1 /* With functions */
+                Optional<Integral> sockets() const noexcept { return Integral{node->first_attribute("sockets")}; }
+                Optional<Integral> cores() const noexcept { return Integral{node->first_attribute("cores")}; }
+                Optional<Integral> threads() const noexcept { return Integral{node->first_attribute("threads")}; }
+#else /* With paramexpr */
+                using sockets(this s) = Integral{node->first_attribute("sockets")};
+                using cores(this s) = Integral{node->first_attribute("cores")};
+                using threads(this s) = Integral{node->first_attribute("threads")};
+#endif
+            };
 
             struct SecModel : public Node {
 #if 1 /* With functions */
@@ -59,13 +79,15 @@ struct DriverCapabilities {
 
 #if 1 /* With functions */
             String arch() const noexcept { return String{node->first_node("arch")}; }
-            // Optional<String> model() const noexcept; // autogen from https://github.com/wiedi/libvirt/blob/master/src/cpu/cpu_map.xml
+            Optional<String> vendor() const noexcept { return String{node->first_node("vendor")}; }
+            Optional<String> model() const noexcept {}
             FeatureList feature_list() const noexcept { return FeatureList{node}; }
             Optional<MigrationFeatures> migration_features() const noexcept { return MigrationFeatures{node->first_node("migration_features")}; }
             Optional<SecModel> secmodel() const noexcept { return SecModel{node->first_node("secmodel")}; }
 #else /* With paramexpr */
             using arch(this s) = String{s.node->first_node("arch")};
-            using model(this s) = /* autogen + TMP magic */;
+            using arch(this s) = String{s.node->first_node("vendor")};
+            using model(this s) = Node{s.node->first_node("model")};
             using feature_list(this s) = FeatureList{s.node};
             using migration_features(this s) = MigrationFeatures{s.node->first_node("migration_features")};
             using secmodel(this s) = SecModel{s.node->first_node("migration_features")};
---
+++ include/virtxml/virtxml.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "capabilities.hpp"
+#include "generic.hpp"
+#include "xmlspan.hpp"
+#include "xmlval.hpp"
