44169df617cdf203a670cf1735cfa2524c84e948
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-08-02 16:55:29 +0200

    Qfix: added sink

--- include/utils.hpp
+++ include/utils.hpp
@@ -14,6 +14,8 @@
 #define UNREACHABLE ;
 #endif
 
+template <class... Ts> void sink(Ts&&... ts) { (static_cast<void>(std::move(ts)), ...); }
+
 template <typename E> constexpr inline decltype(auto) to_integral(E e) { return static_cast<typename std::underlying_type<E>::type>(e); }
 
 constexpr auto pow10(std::size_t n) {
