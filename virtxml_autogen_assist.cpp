55d96eaff6d390589095e60ea73a63e9c0d637dc
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-08-02 12:24:26 +0200

    virtxml autogen: partial progress

--- virtxml_autogen_assist.cpp
+++ virtxml_autogen_assist.cpp
@@ -1,12 +1,15 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <variant>
+#include <flatmap.hpp>
 #include <rapidxml_ns.hpp>
 #include "virtxml/xmlspan.hpp"
 
 using namespace std::literals;
 using namespace rapidxml_ns;
 
+#if 0
 struct StreamCapitalize {
     const char* str;
 };
@@ -44,25 +47,16 @@ enum Marks {
     OneOrMore = 3,
 };
 
-bool verify(const xml_document<>& doc) noexcept {
-    const auto gram_node = doc.first_node();
-    if (!gram_node)
-        return false;
-    if (gram_node->name() != "grammar"sv)
-        return false;
-    if (const auto attr = gram_node->first_attribute("xmlns"); !attr || attr->value() != "http://relaxng.org/ns/structure/1.0"sv)
-        return false;
-    if (const auto attr = gram_node->first_attribute("datatypeLibrary"); !attr || attr->value() != "http://www.w3.org/2001/XMLSchema-datatypes"sv)
-        return false;
-
-    return gram_node->first_node("start");
-}
 
 const xml_node<>& rng_deref(const xml_node<>& n, virtxml::NamedSpan<xml_node<>> definitions) {
     const auto ref_name = n.first_attribute("name")->value();
     const auto it = std::find_if(definitions.begin(), definitions.end(),
                                  [=](const xml_node<>& def) { return std::strcmp(def.first_attribute("name")->value(), ref_name) == 0; });
     return (*it).first_node();
+};
+
+struct DrefRelaxNGIt {
+
 };
 
 void walk(const xml_node<>& n, unsigned marks, gsl::czstring<> scope, virtxml::NamedSpan<xml_node<>> definitions, std::ostream& of) {
@@ -105,13 +99,106 @@ void walk(const xml_node<>& n, unsigned marks, gsl::czstring<> scope, virtxml::N
     } else if (n.name() == "data"sv) {
     }
 }
+#endif
+
+bool verify(const xml_document<>& doc) noexcept {
+    const auto gram_node = doc.first_node();
+    if (!gram_node)
+        return false;
+    if (gram_node->name() != "grammar"sv)
+        return false;
+    if (const auto attr = gram_node->first_attribute("xmlns"); !attr || attr->value() != "http://relaxng.org/ns/structure/1.0"sv)
+        return false;
+    if (const auto attr = gram_node->first_attribute("datatypeLibrary"); !attr || attr->value() != "http://www.w3.org/2001/XMLSchema-datatypes"sv)
+        return false;
+
+    return gram_node->first_node("start");
+}
+
+namespace RNGAST {
+
+class Class;
+class Enum;
+class Property;
+class Datatype;
+
+enum class Type { String, Bool, Int, Unsigned, ReferenceHidden };
+
+struct Class {
+    std::string name;
+    std::vector<Class> classes;
+    std::vector<Property> properties;
+    std::optional<Datatype> dtype;
+};
+
+struct Definition : public Class {};
+
+struct Enum {
+    std::string name;
+    std::vector<std::string> values;
+};
+
+struct Property {
+    std::string name;
+    Type type;
+};
+
+struct Datatype {
+    std::string name;
+    Type type;
+};
+
+constexpr std::array<std::pair<const char*, Type>, 8> builtins{{
+    {"string", Type::String},
+    {"unsignedInt", Type::Unsigned},
+    {"unsignedLong", Type::Unsigned},
+    {"positiveInteger", Type::Unsigned},
+    {"octalMode", Type::String},
+    {"uint8", Type::Unsigned},
+    {"uint16", Type::Unsigned},
+    {"uint24", Type::Unsigned},
+}};
+
+} // namespace RNGAST
+
+void parse(const xml_node<>* gram) {
+    enum class DefTag { NONE, IS_TYPE, IS_CLASSMK };
+
+    constexpr const std::array<std::pair<std::string_view, DefTag>, 7> tags{{{"element", DefTag::IS_CLASSMK},
+                                                                       {"attribute", DefTag::IS_CLASSMK},
+                                                                       {"zeroOrMore", DefTag::IS_CLASSMK},
+                                                                       {"oneOrMore", DefTag::IS_CLASSMK},
+                                                                       {"group", DefTag::IS_CLASSMK},
+                                                                       {"data", DefTag::IS_TYPE},
+                                                                       {"value", DefTag::IS_TYPE}}};
+
+    split_flatmap<std::string_view, DefTag> def_tags;
+
+    // Tag all definitions so we later know what they induce in the AST even if the latter is incomplete
+    for (auto curr = gram->first_node("define"); curr; curr = curr->next_sibling("define")) {
+        auto v = curr;
+        while (v->name() == "interleave"sv || v->name() == "optional"sv || v->name() == "choice"sv)
+            v = v->first_node();
+
+
+        const auto it = std::find_if(tags.begin(), tags.end(), [=](auto tag){ return tag.first == v->name(); });
+        def_tags[curr->first_attribute("name")->value()] = it != tags.end() ? it->second : DefTag::NONE;
+    }
+
+    // ASTify each def while counting references to them
+    // TODO
+
+    // Rebuild full tree, removing singly referenced definitions
+    // TODO
+}
 
 void process(const xml_document<>& doc, std::ofstream of) {
     const auto gram_node = doc.first_node("grammar");
     virtxml::NamedSpan<xml_node<>> definitions{"define", gram_node};
 
     const auto start = gram_node->first_node("start");
-    walk(*start, Marks::None, nullptr, definitions, of);
+    parse(gram_node);
+    // walk(*start, Marks::None, nullptr, definitions, of);
 }
 
 int main(int argc, char** argv) {
