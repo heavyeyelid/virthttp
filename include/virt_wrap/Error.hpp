1a9ff805ec1415415bf4bbf7ef662e5f297e90e0
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-08-12 11:14:19 +0200

    virt_wrap: Added basic support for libvirt error

---
+++ include/virt_wrap/Error.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <string>
+#include <gsl/gsl>
+#include <libvirt/libvirt.h>
+#include <libvirt/virterror.h>
+
+namespace virt {
+class Error;
+class ErrorRef;
+
+ErrorRef getLastError() noexcept;
+Error extractLastError();
+
+class ErrorRef {
+    virErrorPtr underlying;
+    constexpr explicit ErrorRef(virErrorPtr&& u) : underlying(u) {}
+    inline ~ErrorRef() noexcept { virFreeError(underlying); }
+    friend ErrorRef getLastError() noexcept;
+    friend Error extractLastError();
+
+  public:
+    [[nodiscard]] constexpr gsl::czstring<> message() const noexcept { return underlying->message; };
+};
+
+class Error {
+    using Code = virErrorNumber;
+    using Level = virErrorLevel;
+    Code code;
+    Level level;
+    std::string message;
+
+    explicit Error(virErrorPtr p) : code(Code((p ? p->code : 0))), level(p ? p->level : Level(0)), message(p ? p->message : nullptr) {}
+    friend Error extractLastError();
+};
+
+inline ErrorRef getLastError() noexcept { return ErrorRef{virGetLastError()}; }
+inline Error extractLastError() {
+    const auto e = virGetLastError();
+    virResetLastError();
+    return Error{e};
+}
+}
