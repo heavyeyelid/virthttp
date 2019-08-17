cf753dfd0f4617dcf1a49fa7201aa14fb5dac4fe
AeroStun <24841307+AeroStun@users.noreply.github.com>
2019-08-17 21:34:42 +0200

    virt_wrap: fixed Error.hpp
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- include/virt_wrap/Error.hpp
+++ include/virt_wrap/Error.hpp
@@ -14,30 +14,35 @@ Error extractLastError();
 class ErrorRef {
     virErrorPtr underlying;
     constexpr explicit ErrorRef(virErrorPtr&& u) : underlying(u) {}
-    inline ~ErrorRef() noexcept { virFreeError(underlying); }
     friend ErrorRef getLastError() noexcept;
     friend Error extractLastError();
 
   public:
+    inline ~ErrorRef() noexcept { virFreeError(underlying); }
     [[nodiscard]] constexpr gsl::czstring<> message() const noexcept { return underlying->message; };
 };
 
 class Error {
     using Code = virErrorNumber;
     using Level = virErrorLevel;
+
+  public:
     Code code;
     Level level;
     std::string message;
 
+  private:
     inline explicit Error(virErrorPtr p) : code(Code((p ? p->code : 0))), level(p ? p->level : Level(0)), message(p ? p->message : nullptr) {}
-    inline explicit operator bool() const noexcept { return message.c_str() != nullptr; }
     friend Error extractLastError();
+
+  public:
+    inline explicit operator bool() const noexcept { return message.c_str() != nullptr; }
 };
 
 inline ErrorRef getLastError() noexcept { return ErrorRef{virGetLastError()}; }
 inline Error extractLastError() {
-    const auto e = virGetLastError();
+    const auto e = Error{virGetLastError()};
     virResetLastError();
-    return Error{e};
+    return e;
 }
 }
